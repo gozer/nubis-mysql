23a164bada2ce2d17cba7e425f0bdbf0767b302b
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-11-03 14:35:48 -0500

    mysql datadog

---
+++ nubis/puppet/datadog.pp
@@ -0,0 +1,10 @@
+#XXX: duplicated from base
+class { 'datadog_agent':
+  api_key => "%%DATADOG_API_KEY%%",
+}
+
+class { 'datadog_agent::integrations::mysql' :
+ host        => 'localhost',
+ user        => 'root',
+ replication => 1,
+}
