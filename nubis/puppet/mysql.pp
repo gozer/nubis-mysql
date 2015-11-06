97f861e238b56fcf93e4650fc8ed725f0f27abf1
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-11-06 12:02:20 -0500

    fix failover handling

--- nubis/puppet/mysql.pp
+++ nubis/puppet/mysql.pp
@@ -5,3 +5,12 @@ file { '/etc/nubis.d/00-mysql-bootstrap':
     mode    => '0755',
     source  => 'puppet:///nubis/files/mysql-bootstrap',
 }
+
+file { '/usr/local/bin/mysql-failover':
+  ensure => file,
+  owner  => root,
+  group  => root,
+  mode   => '0755',
+  source  => 'puppet:///nubis/files/mysql-failover',
+}
+
